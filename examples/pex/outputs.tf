0d0f8f7995f4e774e1a6a955d036eab0a4ef51c7
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2018-12-22 20:27:28 -0800

    Log python versions used

--- examples/pex/outputs.tf
+++ examples/pex/outputs.tf
@@ -2,3 +2,8 @@ output "command_echo" {
   description = "For the pex data source, if successful, this will contain the echo string."
   value       = "${lookup(module.pex_data.result, "echo")}"
 }
+
+output "command_python_version" {
+  description = "Read out the python version that was used to execute the PEX"
+  value       = "${lookup(module.pex_data.result, "python_version_info")}"
+}
