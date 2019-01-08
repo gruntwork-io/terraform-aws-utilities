b0c71891a33180febc86c8e57216721768a8c8c9
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2019-01-07 19:55:50 -0800

    Unit test

--- modules/require-executable/outputs.tf
+++ modules/require-executable/outputs.tf
@@ -1,5 +1,4 @@
 output "executables" {
   description = "A map of the executables to the resolved path where they reside."
-  value       = "${data.external.required_executables.result}"
-  type        = "map"
+  value       = "${data.external.required_executable.result}"
 }
