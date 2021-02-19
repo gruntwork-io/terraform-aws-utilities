f37f392f2a2116e42dd73062f75b9080323c4f6f
Ana Krivokapic <ana@gruntwork.io>
2021-02-19 10:21:08 +0100

    Upgrade to Terraform 0.14

--- modules/prepare-pex-environment/dependencies.tf
+++ modules/prepare-pex-environment/dependencies.tf
@@ -4,7 +4,10 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 terraform {
-  required_version = ">= 0.12"
+  # This module is now only being tested with Terraform 0.14.x. However, to make upgrading easier, we are setting
+  # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
+  # forwards compatible with 0.14.x code.
+  required_version = ">= 0.12.26"
 }
 
 # Figure out the installed and configured python version
