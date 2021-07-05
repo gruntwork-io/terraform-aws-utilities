fea10aac2bb3b30be97e71b0272b52333e6105b2
Ina <ina.m.stoyanova@gmail.com>
2021-07-05 20:09:52 +0100

    run pre-commit

--- test/instance_type_test.go
+++ test/instance_type_test.go
@@ -1,9 +1,10 @@
 package test
 
 import (
+	"testing"
+
 	"github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/gruntwork-io/terratest/modules/terraform"
-	"testing"
 )
 
 func TestInstanceType(t *testing.T) {
