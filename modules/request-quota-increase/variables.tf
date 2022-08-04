1e19c1e28ab75b5e0c24b5d4c3e4a5c67d2c7d92
Marina Limeira <marina@gruntwork.io>
2022-08-04 15:23:04 +0200

    improve description of variable

--- modules/request-quota-increase/variables.tf
+++ modules/request-quota-increase/variables.tf
@@ -1,5 +1,5 @@
 variable "resources_to_increase" {
-  description = "A map where the key is the resource and the value is the desired quota. The only services supported at the moment are VPC and IAM. If you need other resources, you can use the generate_code.rb script to add more resources and contribute to this module."
+  description = "A map where the key is the resource and the value is the desired quota. The only services supported at the moment are VPC and IAM. If you need other resources, PRs to add support for other services are very welcome. See the generate_code.rb script for details on how."
   type        = map(number)
 
   # Example:
