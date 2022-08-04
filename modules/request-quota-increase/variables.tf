e38d01c88982c14978db1a79196a3ea47928146c
Marina Limeira <marina@gruntwork.io>
2022-08-04 15:20:51 +0200

    improve description of variable

--- modules/request-quota-increase/variables.tf
+++ modules/request-quota-increase/variables.tf
@@ -1,10 +1,10 @@
 variable "resources_to_increase" {
-  description = "A map where the key is the resource and the value is the desired quota. The only services supported at the moment are VPC and IAM. You can also use the `aws_servicequotas_service_quota` resource directly!"
+  description = "A map where the key is the resource and the value is the desired quota. The only services supported at the moment are VPC and IAM. If you need other resources, you can use the generate_code.rb script to add more resources and contribute to this module."
   type        = map(number)
 
   # Example:
   # {
-  # vpc_ipv4_cidr_blocks_per_vpc     = 10,
-  # vpc_internet_gateways_per_region = 15,
+  #   vpc_ipv4_cidr_blocks_per_vpc     = 10,
+  #   vpc_internet_gateways_per_region = 15,
   # }
 }
