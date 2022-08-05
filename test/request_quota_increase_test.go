43ac0d48a4e00b020e3baa189276958e831118ae
Marina Limeira <marina@gruntwork.io>
2022-08-05 15:40:55 +0200

    goimports

--- test/request_quota_increase_test.go
+++ test/request_quota_increase_test.go
@@ -3,7 +3,6 @@ package test
 import (
 	"testing"
 
-	"github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 )
