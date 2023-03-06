63b1195794b0ce24d6b225e2ef66579ca52b6d43
Max Moon <moon.maxwell@gmail.com>
2023-03-06 11:06:57 -0800

    chore: pin hashi external provider to 2.2.3 (#72)

--- modules/require-executable/main.tf
+++ modules/require-executable/main.tf
@@ -1,6 +1,18 @@
 terraform {
   # This module is now only being tested with Terraform 1.1.x. However, to make upgrading easier, we are setting 1.0.0 as the minimum version.
   required_version = ">= 1.0.0"
+
+  # Updating the Terraform external provider to 2.3.0 caused an undocumented breaking change (as evidenced by
+  # issues like https://github.com/hashicorp/terraform-provider-external/issues/193). The solution is to pin 
+  # the version to a working version for now. 
+  # Special thanks to Lorelei Rupp for reporting and discovering the root cause of this issue!
+  # TODO: Once the issue is fixed. Update the version constraint to the latest non-breaking version.
+  required_providers {
+    external = {
+      source  = "hashicorp/external"
+      version = "= 2.2.3"
+    }
+  }
 }
 
 data "external" "required_executable" {
