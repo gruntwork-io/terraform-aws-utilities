6d1f4a3752365cc2385fcb39b970830c7f2e9861
Ryan Russell <ryan@gruntwork.io>
2023-10-12 10:02:56 -0700

    Revert Pin Terraform version - [CORE-1297] (#89)

--- examples/list-remove/main.tf
+++ examples/list-remove/main.tf
@@ -1,8 +1,5 @@
 terraform {
-  # Due to the HashiCorp BSL License change, we are restricting the latest version of Terraform to 1.5.7.
-  # Going forward we will be adding support for OpenTofu. For additional information on OpenTofu please 
-  # see the official OpenTofu website at https://opentofu.org/.
-  required_version = ">= 1.0.0, <= 1.5.7"
+  required_version = ">= 1.0.0"
 }
 
 module "list_remove" {
